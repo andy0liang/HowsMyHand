49621ab2daa439b55234b45506af1ec85281213c
Andy Liang <andy0liang@gmail.com>
2020-07-29 19:14:10 -0700

    skeleton functionality

--- src/Cards.java
+++ src/Cards.java
@@ -47,5 +47,41 @@ public class Cards {
         return name;
     }
 
+    //Diamonds, clubs, hearts, spades
+    //   0        1      2        3
+    public static int cardID(String card) {
+        char value = card.charAt(0);
+        char suit = card.charAt(1);
+        int id = 0;
+        if (value == 'a' || value == 'A') {
+            id += 0;
+        } else if (value == 'T' || value == 't') {
+            id += 9;
+        } else if (value == 'J' || value == 'j') {
+            id += 10;
+        } else if (value == 'Q' || value == 'q') {
+            id += 11;
+        } else if (value == 'K' || value == 'k') {
+            id += 12;
+        } else {
+            id += (value - '0') - 1;
+        }
+        if (suit == 'd' || suit == 'D') {
+            id += (13 * 0);
+        }
+        if (suit == 'c' || suit == 'C') {
+            id += (13 * 1);
+        }
+        if (suit == 'h' || suit == 'H') {
+            id += (13 * 2);
+        }
+        if (suit == 's' || suit == 'S') {
+            id += (13 * 3);
+        }
+
+        return id;
+
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.util.*;
 public class Main {
-
+public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
+        //Diamonds, clubs, hearts, spades
+        //   0        1      2        3
 
         while (true) {
             System.out.println("New Hold'em hand to analyze. Enter starting cards:");
@@ -11,8 +13,51 @@ public class Main {
             String n1 = Cards.cardName(c1);
             String n2 = Cards.cardName(c2);
 
-            System.out.println(n1 + "\n" + n2);
+            System.out.println("How many players are you playing against?");
+            int numOthers = askPlayers();
+
+            //calculate pre-flop
+
+            System.out.println("Enter flop:");
+            String f1 = input.nextLine();
+            if (f1.equals("gg")) {
+                continue;
+            }
+            String f2 = input.nextLine();
+            String f3 = input.nextLine();
+
+            numOthers = askPlayers();
+
+            //calculate
+
+            System.out.println("Enter 4th card:");
+            String f4 = input.nextLine();
+            if (f4.equals("gg")) {
+                continue;
+            }
+            numOthers = askPlayers();
+
+            //calculate
+
+            System.out.println("Enter 5th card:");
+            String f5 = input.nextLine();
+            if (f5.equals("gg")) {
+                continue;
+            }
+            numOthers = askPlayers();
+
+            //calculate
+
+            System.out.println("Press enter for new game");
+            String lmao = input.nextLine();
 
         }
+    }
+
+
+    public static int askPlayers() {
+        int num = input.nextInt();
+        String dummy = input.nextLine();
+        return num;
     }
 }
